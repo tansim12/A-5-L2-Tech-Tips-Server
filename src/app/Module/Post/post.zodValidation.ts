e9eca96a1a92929a1576097c69f022fee18ab8f3
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2024-10-08 11:25:59 +0600

    description limit extend

--- src/app/Module/Post/post.zodValidation.ts
+++ src/app/Module/Post/post.zodValidation.ts
@@ -18,7 +18,7 @@ const createPostZodSchema = z.object({
     description: z
       .string()
       .nonempty("Description is required")
-      .max(2000, "Description cannot exceed 2000 characters"),
+      .max(5000, "Description cannot exceed 5000 characters"),
     images: z.array(z.string()).optional(), // Optional array of image URLs
     react: z.array(z.string()).optional(), // Optional array of user IDs who reacted
     comments: z.array(z.string()).optional(), // Optional array of comments
